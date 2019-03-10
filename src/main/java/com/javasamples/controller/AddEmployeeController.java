4ce959917effc403d19cffac8fc8da2b17e70724
Kiran Sripada <kiran.sripada@sherwin.com>
2019-03-10 17:32:32 -0400

    Spring Forms

--- src/main/java/com/javasamples/controller/AddEmployeeController.java
+++ src/main/java/com/javasamples/controller/AddEmployeeController.java
@@ -2,7 +2,9 @@ package com.javasamples.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -15,7 +17,7 @@ public class AddEmployeeController {
 	@Autowired
 	private EmployeeService employeeService;
 
-	@RequestMapping("/addNewEmployee")
+/*	@RequestMapping("/addNewEmployee")
 	public ModelAndView addEmployee(@RequestParam("empId") String empId,
 			@RequestParam("name") String name,
 			@RequestParam("designation") String designation,
@@ -24,7 +26,26 @@ public class AddEmployeeController {
 		Employee employee = new Employee(empId, name, designation, salaryDouble);
 		employeeService.addNewEmployee(employee);
 		return new ModelAndView("/employee-added.jsp", "name", name);
+	}*/
+	
+	@RequestMapping(value = "/addNewEmployee", method = RequestMethod.GET)
+	public ModelAndView show() {
+		System.out.println("KIRAN -- show()");
+		return new ModelAndView("/addEmployee.jsp", "emp", new Employee());
 	}
+	
+	@RequestMapping(value = "/addNewEmployee", method = RequestMethod.POST)
+	public ModelAndView processRequest(Employee emp, Errors result) {
+		System.out.println("KIRAN -- processRequest()");
+
+		if (result.hasErrors()) {
+			return new ModelAndView("/addEmployee.jsp", "emp", emp);
+		}
+		employeeService.addNewEmployee(emp);
+		return new ModelAndView("/employee-added.jsp", "name", emp.getName());
+	}
+
+
 
 
 }
--- src/main/webapp/addEmployee.jsp
+++ src/main/webapp/addEmployee.jsp
@@ -1,13 +1,15 @@
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
 <%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt"%>
 <%@ taglib prefix="fn" uri="http://java.sun.com/jsp/jstl/functions"%>
+<%@taglib uri="http://www.springframework.org/tags/form" prefix="form"%>
+
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 <title>Add Employee	</title>
 </head>
 <body>
-<div id="addItem">
+<%-- <div id="addItem">
 		<form action="addNewEmployee.do" method="post">
 			<label>Enter Employee Id</label><input type="text" name="empId" /><br>
 			<label>Enter Employee Name</label><input type="text" name="name" /><br>
@@ -17,6 +19,28 @@
 				 <label>Enter Employee Salary</label>
 				<input type="text" name="salary" /><br> <input type="submit"value="Add" />
 		</form>
+	</div> --%>
+	
+	<div id="addEmployee">
+		<form:form action="addNewEmployee.do" method="post" commandName="emp">
+			<p>
+				<label>Enter Employee Id<fmt:message key="emp.empId" /></label>
+				<form:input path="empId" />
+			</p>
+			<p>
+				<label>Enter Name<fmt:message key="emp.name" /></label>
+				<form:input path="name" />
+			</p>
+			<p>
+				<label>Enter Type<fmt:message key="emp.designation" /></label>
+				<form:input path="designation" />
+			</p>
+			<p>
+				<label>Enter Price<fmt:message key="emp.salary" /></label>
+				<form:input path="salary" />
+			</p>
+			<input type="submit" value="Add New Employee" />
+		</form:form>
 	</div>
 
 </body>
